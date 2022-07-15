f87ab9f01586ce07368886ff03fc5dd1e6bffd75
wanjuhiann <106369744+wanjuhiann@users.noreply.github.com>
2022-07-15 14:04:39 +0300

    Add files via upload

---
+++ Ann assigment payroll.c
@@ -0,0 +1,48 @@
+/*
+GITONGA FACTORY
+Created by ANN NYAMBURA DIT-01-8862/2021
+jULY2022
+C89
+MIT LICENCE
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int totalhoursWorked-40
+    int kraPin, emloyment No, hours worked, normalpay, overtimepay, taxpaid, netincome;
+    char name(100);
+
+    printf("Welcome to Gitonga's factory\n");
+
+    printf("Enter your name:");
+    scanf("%d &name");
+
+     printf("Enter your employee number:");
+     scanf("%d &employment No");
+
+
+     printf("Enter your KRA Pin:");
+     scanf("%d &kra pin");
+
+     printf("Enter number of hours worked:");
+     scanf("%d &hours worked");
+
+     normalpay-1000 *totalhoursWorked)
+     overtimepay=(hoursworked-totalhourworked) *1500;
+     taxpaid-normalpay  *30/100
+     netincome =(normalpay - taxpaid) + overtimepay;
+
+
+      printf("normalpay:%d/n",normalpay);
+      printf("overtimepay:%d/n",overtimepay);
+      printf("taxpaid:%d/n", taxpaid);
+       printf("net income:%d/n",netincome);
+
+       return 0;
+
+
+    return 0;
+}
+/
